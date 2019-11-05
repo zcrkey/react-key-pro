3116fa70631400a5db662e325d1ddbde31af8433
zcrkey <774508556@qq.com>
2019-11-05 20:56:39 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,22 @@
+# React
+
+### 创建项目
+    npm init react-app react-key
+
+### 引入 scss
+    npm install sass-loader node-sass --save-dev
+
+
+
+
+
+
+
+
+
+
+
+
+
+### 暴露配置文件
+    npm run eject
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="key-app">
+      <div className="key-app-header">
+        <div className="key-app-logo">
+          Logo
+        </div>
+        <div className="key-app-menu">
+          菜单栏
+        </div>
+        <div className="key-app-user">
+          信息
+        </div>
+      </div>
+      <div className="key-app-body">
+        <div className="key-app-sidebar">
+          侧边栏
+        </div>
+        <div className="key-app-main">
+          <div className="key-app-tab">
+            标签栏
+          </div>
+          <div className="key-app-content">
+            内容区
+            <div style={{ height: 900 }}>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
