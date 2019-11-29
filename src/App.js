60a3dd99358cc5522a0cb073d96d800ba6acbdc5
zcrkey <774508556@qq.com>
2019-11-29 13:29:13 +0800

    引入 fortawesome 图标库

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// 引入 fortawesome 图标库
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 import Main from './project/main/main';
 import Login from './project/login/login';
 import Register from './project/register/register';
 import NoMatch from './project/no-match/noMatch';
+
+// 全部引入
+library.add(fas);
 
 function App() {
   return (
--- src/project/fontawesome/fontawesome.js
+++ src/project/fontawesome/fontawesome.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const styles = {
+  faIcon: {
+    color: '#495057',
+    textAlign: 'center',
+    width: '5%',
+    fontSize: '28px'
+  }
+};
 
 export default class Fontawesome extends React.Component {
 
@@ -10,8 +20,21 @@ export default class Fontawesome extends React.Component {
   render() {
     return (
       <div>
-        <h4>Fontawesome</h4>
+        <h4><a href='http://fa5.dashgame.com/#/%E5%9B%BE%E6%A0%87' target='_blank'>Fontawesome - All 1544 Awesome Icons</a></h4>
+        <FontAwesomeIcon style={styles.faIcon} icon="align-center" />
+        <FontAwesomeIcon style={styles.faIcon} icon="align-justify" />
+        <FontAwesomeIcon style={styles.faIcon} icon="align-left" />
+        <FontAwesomeIcon style={styles.faIcon} icon="align-right" />
+        <FontAwesomeIcon style={styles.faIcon} icon="angle-double-down" />
+        <FontAwesomeIcon style={styles.faIcon} icon="angle-double-up" />
+        <FontAwesomeIcon style={styles.faIcon} icon="angle-double-left" />
+        <FontAwesomeIcon style={styles.faIcon} icon="angle-double-right" />
+        <FontAwesomeIcon style={styles.faIcon} icon="check-square" />
+        <FontAwesomeIcon style={styles.faIcon} icon="coffee" />
+        <FontAwesomeIcon style={styles.faIcon} icon="comment-alt" />
+        <FontAwesomeIcon style={styles.faIcon} icon="ad" />
       </div>
     )
   }
 }
+
