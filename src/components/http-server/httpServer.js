1d85af75d64ea03200206cef1687f3a73aaaea74
zcrkey <774508556@qq.com>
2019-12-04 17:25:09 +0800

    请求服务

---
+++ src/components/http-server/httpServer.js
@@ -0,0 +1,21 @@
+export default class HttpServer {
+
+  /**
+   * 获取基础地址
+   */
+  static getBaseUrl() {
+    // 综治 demo api
+    let baseUrl = 'http://10.0.7.205:8080/demozz.api';
+    return baseUrl;
+  }
+
+  /**
+   * 处理响应数据
+   */
+  static handleResponse(response) {
+
+  }
+
+  
+
+}
---
+++ src/project/event/server.js
@@ -0,0 +1,21 @@
+// TODO：是否需要将服务提取为独立类
+
+export default class EventServer {
+
+  static list() {
+
+  }
+
+  static get() {
+
+  }
+
+  static save() {
+
+  }
+
+  static edit() {
+
+  }
+
+}
