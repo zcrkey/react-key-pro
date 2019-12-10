19b622166538b50e2528549f60879272b255b5f0
zcrkey <774508556@qq.com>
2019-12-10 14:46:52 +0800

    upload

--- src/project/request/request.js
+++ src/project/request/request.js
@@ -21,6 +21,9 @@ export default class Request extends React.Component {
   async getRequest() {
     let result = await HttpServer.get('/api/sys/init');
     console.log(result);
+    if (!!result) {
+      await GlobalDataUtils.init(result);
+    }
   }
 
   async postRequest() {
@@ -59,19 +62,33 @@ export default class Request extends React.Component {
     console.log(result);
   }
 
+  async onUpload(e) {
+    if (e.currentTarget.files && e.currentTarget.files.length > 0) {
+      let files = e.currentTarget.files[0];
+      let result = await HttpServer.upload(files, {
+        onUploadProgress: (progressEvent) => {
+          console.log(progressEvent);
+        }
+      });
+      console.log(result);
+    }
+  }
+
   render() {
     return (
       <div>
         <h4 style={styles.title}>Request</h4>
         token:{this.state.token}
         <br />
-        <button onClick={() => { this.getRequest() }}>get 请求</button>
+        <button onClick={() => { this.getRequest() }}>get 请求 - init</button>
         <br />
         <button onClick={() => { this.postRequest() }}>post 请求</button>
         <br />
-        <button onClick={() => { this.postJsonRequest() }}>postJson 请求</button>
+        <button onClick={() => { this.postJsonRequest() }}>postJson 请求 - loginIn</button>
         <br />
         <button onClick={() => { this.allRequest() }}>all 请求</button>
+        <br />
+        <input type="file" onChange={(e) => { this.onUpload(e) }}></input>
       </div >
 
     )
--- src/utils/httpServer.js
+++ src/utils/httpServer.js
@@ -24,12 +24,101 @@ export default class HttpServer {
   }
 
   /**
+   * 获取后缀名
+   * @param {*} filePath 
+   */
+  static getSuffixName(filePath) {
+    let suffixName = '';
+    if (!!filePath) {
+      let index = filePath.lastIndexOf(".");
+      if (index > -1) {
+        var length = filePath.length + '';
+        suffixName = (filePath.substring(index, length)).toLowerCase();
+      }
+    }
+    return suffixName;
+  }
+
+  /**
+   * 根据后缀名获取上传类型接口
+   * @param {*} suffixName 
+   */
+  static getUploadTypeApi(suffixName) {
+    let uploadUrl = '';
+    // 判断是否为图片类型，图片类型有特定上传接口
+    let imageType = ['png', 'jpg', 'jpeg', 'bmp', 'gif', 'webp', 'psd', 'svg', 'tiff'];
+    // 判断是否为音频类型，音频类型有特定上传接口
+    let audioType = ['aac', 'mp3', 'wav', 'ogg'];
+    if (imageType.indexOf(suffixName) !== -1) {
+      uploadUrl = GlobalDataUtils.getGlobalUrl().uploadFile + '/api/file/uploadImage';
+    } else if (audioType.indexOf(suffixName) !== -1) {
+      uploadUrl = GlobalDataUtils.getGlobalUrl().uploadFile + '/api/file/upload/voice';
+    } else {
+      uploadUrl = GlobalDataUtils.getGlobalUrl().uploadFile + '/api/file/upload';
+    }
+    return uploadUrl;
+  }
+
+  /**
    * 处理响应数据
    * @param {*} response 
+   * @param {*} fail 
    */
-  static handleResponse(response) {
+  static handleResponse(response, fail) {
     if (response.status) {
-      return true;
+      if (response.code == '' || response.code == '200') {
+        return true;
+      }
+      if (response.code == '201') {
+        this.addErrorMessage();
+        // Dialog.fail('没有权限请求！', {
+        //   close: function () {
+        //     Actions.reset('LoginPage', {
+        //       isLoginOut: true
+        //     });
+        //   }
+        // });
+        return false;
+      }
+      if (response.code == '205') {
+        // Dialog.fail('系统已过期！', {
+        //   close: function () {
+        //     Actions.reset('LoginPage', {
+        //       isLoginOut: true
+        //     });
+        //   }
+        // });
+        return false;
+      }
+      if (response.code == '202' || response.code == '203') {
+        // Dialog.fail('未登录或登录状态过期，请重新登录！', {
+        //   close: function () {
+        //     Actions.reset('LoginPage', {
+        //       isLoginOut: true
+        //     });
+        //   }
+        // });
+        return false;
+      }
+      if (response.code == '204') {
+        // Dialog.fail('检测你的帐号在别的地方登录，请重新登录！', {
+        //   close: function () {
+        //     Actions.reset('LoginPage', {
+        //       isLoginOut: true
+        //     });
+        //   }
+        // });
+        return false;
+      }
+      let code = GlobalDataUtils.getCode(response.code);
+      if (code) {
+        // Dialog.fail(code.message);
+        return false;
+      }
+      if (fail) {
+        fail();
+      }
+      return false;
     } else {
       return false;
     }
@@ -67,9 +156,9 @@ export default class HttpServer {
    */
   static createUploadInstance(settings) {
     let options = {
-      contentType: 'multipart/form-data',
+      contentType: 'multipart/form-data', // 'application/octet-stream' 下载
       timeout: 30 * 1000,
-      onUploadProgress: () => { }
+      onUploadProgress: (progressEvent) => { }
     };
     options = Object.assign(options, settings);
     let instance = Axios.create({
@@ -84,6 +173,34 @@ export default class HttpServer {
       onUploadProgress: (progressEvent) => {
         console.log(progressEvent);
         options.onUploadProgress(progressEvent);
+      }
+    });
+    return instance;
+  }
+
+  /**
+   * 创建下载实例
+   * @param {*} settings {contentType,timeout、onDownloadProgress}
+   */
+  static createDownloadInstance(settings) {
+    let options = {
+      contentType: 'application/octet-stream',
+      timeout: 30 * 1000,
+      onDownloadProgress: () => { }
+    };
+    options = Object.assign(options, settings);
+    let instance = Axios.create({
+      baseURL: this.getBaseUrl(),
+      timeout: options.timeout,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': options.contentType,
+        'zeus-token': GlobalDataUtils.getToken()
+      },
+      responseType: 'json',
+      onDownloadProgress: (progressEvent) => {
+        console.log(progressEvent);
+        options.onDownloadProgress(progressEvent);
       }
     });
     return instance;
@@ -192,8 +309,42 @@ export default class HttpServer {
     }
   }
 
-  static async upload() {
-
+  /**
+   * 上传
+   * @param {*} files
+   * @param {*} settings {type,name,suffixName}
+   */
+  static async upload(files, settings) {
+    try {
+      if (!!files) {
+        let options = {
+          onUploadProgress: (progressEvent) => { }
+        };
+        options = Object.assign(options, settings);
+        let instance = this.createUploadInstance({
+          onUploadProgress: options.onUploadProgress
+        });
+        const formData = new FormData();
+        // 这里的 file 与后台相对应，不能改变
+        formData.append('file', files);
+        let suffixName = this.getSuffixName(files.name);
+        let url = this.getUploadTypeApi(suffixName);
+        // put 、 post 的区别（PUT请求：如果两个请求相同，后一个请求会把第一个请求覆盖掉。Post请求：后一个请求不会把第一个请求覆盖掉。）
+        let response = await instance.put(url, formData);
+        let responseData = response.data;
+        let status = this.handleUploadResponse(responseData);
+        return new Promise((resolve, reject) => {
+          if (status) {
+            resolve(responseData.data);
+          } else {
+            resolve();
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
+
 
 }
